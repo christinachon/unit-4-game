4ef0216f95f3429ee3b24ad467923232c4d78efc
Christina <christinachon90@gmail.com>
2019-06-15 13:59:10 -0700

    changes images to buttons to add a value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,34 @@
 var wins = "0";
 var losses = "0";
-var targetScore = Math.floor(Math.random()* 100) + 21
+var targetScore;
 var myScore;
+var greenmush;
+var redmush;
+var posionmush;
+var goldmush;
+
 
 function newGame(){
     targetScore = Math.floor(Math.random()* 100) + 21;
     myScore = 0;
+    var greenmush = Math.floor(Math.random()*12)+1;
+    var redmush = Math.floor(Math.random()*12)+1;
+    var poisonmush = Math.floor(Math.random()*12)+1;
+    var goldmush = Math.floor(Math.random()*12)+1;
     $("h3.targetscore").html(targetScore);
     $("h3.score").html(myScore);
     $("div.wins").html(wins);
     $("div.losses").html(losses);
+    $('.greenmush').attr('value', greenmush);
+    $('.redmush').attr('value', redmush);
+    $('.poisonmush').attr('value', poisonmush);
+    $('.goldmush').attr('value', goldmush);
 }
 // MAIN GAME PART
 newGame();
 
 $(".mush").click(function(){
-    mushValue = Math.floor(Math.random()*12)+1
-    myScore += mushValue;
+    myScore += parseInt($(this).val());
     $("h3.score").html(myScore);
     if (myScore > targetScore){
         losses++;
@@ -25,5 +37,6 @@ $(".mush").click(function(){
     } else if(myScore === targetScore){
         wins++;
         $("div.wins").html(wins);
+        newGame();
     };
 });
