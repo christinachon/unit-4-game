4401a1d08981160d6c459b767b4f34b5ecbab7d3
Christina <christinachon90@gmail.com>
2019-06-14 19:54:25 -0700

    added win and lose conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
-var wins = 0;
-var losses = 0;
+var wins = "0";
+var losses = "0";
 var targetScore = Math.floor(Math.random()* 100) + 21
 var myScore;
 
 function newGame(){
-    targetScore;
+    targetScore = Math.floor(Math.random()* 100) + 21;
     myScore = 0;
     $("h3.targetscore").html(targetScore);
+    $("h3.score").html(myScore);
+    $("div.wins").html(wins);
+    $("div.losses").html(losses);
 }
 // MAIN GAME PART
 newGame();
@@ -15,19 +18,52 @@ $(".gold").click(function(){
     goldValue = Math.floor(Math.random()*12)+1
     myScore += goldValue;
     $("h3.score").html(myScore);
+    if (myScore > targetScore){
+        losses++;
+        $("div.losses").html(losses);
+        newGame();
+    } else if(myScore === targetScore){
+        wins++;
+        $("div.wins").html(wins);
+    };
 });
 $(".green").click(function(){
     greenValue = Math.floor(Math.random()*12)+1
     myScore += greenValue;
     $("h3.score").html(myScore);
+    if (myScore > targetScore){
+        losses++;
+        $("div.losses").html(losses);
+        newGame();
+    } else if(myScore === targetScore){
+        wins++;
+        $("div.wins").html(wins);
+    };
 });
 $(".poison").click(function(){
     poisonValue = Math.floor(Math.random()*12)+1
     myScore += poisonValue;
     $("h3.score").html(myScore);
+    if (myScore > targetScore){
+        losses++;
+        $("div.losses").html(losses);
+        newGame();
+    } else if(myScore === targetScore){
+        wins++;
+        $("div.wins").html(wins);
+    };
 });
 $(".red").click(function(){
     redValue = Math.floor(Math.random()*12)+1
     myScore += redValue;
     $("h3.score").html(myScore);
+    if (myScore > targetScore){
+        losses++;
+        $("div.losses").html(losses);
+        newGame();
+    } else if(myScore === targetScore){
+        wins++;
+        $("div.wins").html(wins);
+    };
 });
+
